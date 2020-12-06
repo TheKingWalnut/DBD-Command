a28642dde215f56cbeeb980f7d3824c3c406a74b
TheKingWalnut <75557628+TheKingWalnut@users.noreply.github.com>
2020-12-06 02:59:09 -0500

    Update values.py

--- values.py
+++ values.py
@@ -1,4 +1,4 @@
-killers = {'Trapper': 0, 'Wraith': 0, 'Hillbilly': 0, 'Nurse': 0, 'Shape': 0, 'Hag': 0, 'Doctor': 0, 'Huntress': 0, 'Cannibal': 0, 'Nightmare': 0, 'Pig': 0, 'Clown': 0, 'Spirit': 0, 'Legion': 0, 'Plague': 0, 'Ghost Face': 0, 'Demogorgon': 0, 'Oni': 0, 'Deathslinger': 0, 'Executioner': 0, 'Blight': 0, 'Twins': 0}
+killers = {'trapper': 0, 'wraith': 0, 'hillbilly': 0, 'nurse': 0, 'shape': 0, 'hag': 0, 'doctor': 0, 'huntress': 0, 'cannibal': 0, 'nightmare': 0, 'pig': 0, 'clown': 0, 'spirit': 0, 'legion': 0, 'plague': 0, 'ghostface': 0, 'demogorgon': 0, 'oni': 0, 'deathslinger': 0, 'executioner': 0, 'blight': 0, 'twins': 0}
 pb = [0]
 #THIS FILE **MUST** BE DRAGGED INTO THE FOLLOWING LOCATION (could be different if you don't have it in the default place, but once you're in the Chatbot folder it's the same)
 #C:\Users\USERNAME\AppData\Roaming\Streamlabs\Streamlabs Chatbot\Lib
