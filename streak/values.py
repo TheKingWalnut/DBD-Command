ee7f8b8bec17535a5d72f0809c4f14b2d72a7d2e
TheKingWalnut <75557628+TheKingWalnut@users.noreply.github.com>
2020-12-06 01:19:04 -0500

    Create values.py
    THIS FILE MUST GO IN THE FOLLOWING LOCATION (may change on where streamlabs chatbot is)
    C:\Users\USERNAME\AppData\Roaming\Streamlabs\Streamlabs Chatbot\Lib
    Just drag the file in to that folder and everything should work. IF YOU DO NOT DO THIS, IT WILL NOT WORK

---
+++ streak/values.py
@@ -0,0 +1,2 @@
+killers = {'Trapper': 0, 'Wraith': 0, 'Hillbilly': 0, 'Nurse': 0, 'Shape': 0, 'Hag': 0, 'Doctor': 0, 'Huntress': 0, 'Cannibal': 0, 'Nightmare': 0, 'Pig': 0, 'Clown': 0, 'Spirit': 0, 'Legion': 0, 'Plague': 0, 'Ghost Face': 0, 'Demogorgon': 0, 'Oni': 0, 'Deathslinger': 0, 'Executioner': 0, 'Blight': 0, 'Twins': 0}
+pb = [0]
